1b8b650f69f21cdc7029396c54a320894d944dea
sly <shaoly1122@vip.sina.com>
2017-09-20 15:05:58 +0800

    change Hello.java and add action.java in dev branch

--- Hello.java
+++ Hello.java
@@ -1,8 +1,5 @@
 public class Hello{
 	public static void main(String args[]){
-		System.out.println("Hello 1");
-		System.out.println("Hello 2");
-		System.out.println("Hello 2");
-		System.out.println("Hello 3");
+		Date date = new Date();
 	}
 }
